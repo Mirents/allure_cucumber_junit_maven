993d76f52a08585c3174e3471ec5f6ab3d100dc0
Mirents <mirent@mail.ru>
2022-01-28 01:31:54 +0300

    Add new Steps

--- src/test/java/com/dws/steps/BaseSteps.java
+++ src/test/java/com/dws/steps/BaseSteps.java
@@ -1,9 +1,7 @@
 package com.dws.steps;
 
 import com.dws.managers.PageManager;
-import com.dws.managers.PropertiesManager;
 
 public class BaseSteps {
     protected PageManager apptest = PageManager.getPageManager();
-    protected PropertiesManager propertiesManager = PropertiesManager.getPropertiesManager();
 }
--- src/test/java/com/dws/steps/MenuSteps.java
+++ src/test/java/com/dws/steps/MenuSteps.java
@@ -4,10 +4,35 @@ import io.cucumber.java.ru.И;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class TopMenuSteps {
-    @И("^в верхнем меню пользователь вводит текст \"([^\"]*)\"$")
-    public void enterTextToFindInput(String text) {
-        log.info("enterTextToFindInput");
+public class MenuSteps extends BaseSteps {
+    @И("^в левом меню нажать \"([^\"]*)\"$")
+    public void clickToLeftMenuStep(String text) {
+        log.info("В левом меню нажать на '{}'", text);
+        apptest
+                .getMenuToPage()
+                .clickLeftMenu(text);
+    }
+    
+    @И("^в верхнем меню в категории \"([^\"]*)\" выбрать \"([^\"]*)\"$")
+    public void clickToSubcategoriesInTopMenu(String categories, String subCategories) {
+        apptest
+                .getMenuToPage()
+                .mouseMoveToTopMenu(categories)
+                .clickTopSubMenu(subCategories);
+    }
+    
+    @И("^в верхнем меню проверить соответствие количества товаров в корзине$")
+    public void assertCartQuanity() {
+        apptest
+                .getMenuToPage()
+                .assertLabelShoppingCartQuantity();
+    }
+    
+    @И("^в верхнем меню нажать на кнопку перехода в корзину$")
+    public void goToCart() {
+        apptest
+                .getMenuToPage()
+                .goToCart();
     }
     
     @И("^в верхнем меню нажать кнопку согласия выбора города$")
